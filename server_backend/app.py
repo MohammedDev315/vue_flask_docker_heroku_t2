447616bfda841f4013a848134db3a11801d0a340
MohammedDev315 <mmohammaddev33@gmail.com>
2022-08-01 12:43:52 +0300

    title changed

--- server_backend/app.py
+++ server_backend/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/' , methods=['GET'])
 def home():
-    return 'welcome from vue flask app test'
+    return 'welcome from vue flask app test 22'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
