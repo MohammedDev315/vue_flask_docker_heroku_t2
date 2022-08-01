552d0f90b11c850fcacb9c6f47db548f2c50f730
MohammedDev315 <mmohammaddev33@gmail.com>
2022-08-01 12:27:57 +0300

    re build

---
+++ server_backend/app.py
@@ -0,0 +1,11 @@
+from crypt import methods
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/' , methods=['GET'])
+def home():
+    return 'welcome from vue flask app test'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
